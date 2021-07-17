d686c0d26a1693484d6f74fa0ac26e073f4a1d73
SeongminHa <seongminha0910@gmail.com>
2021-07-17 18:08:00 +0000

    Fix: URL 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about ...`,
   author: `Seongmin Ha`,
   introduction: `휘발되지 않는 멋쟁이 개발자 되기`,
-  siteUrl: `https://makeitmin.io`, // Your blog site url
+  siteUrl: `https://makeitmin.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `https://www.github.com/makeitmin`, // Your GitHub account
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'makeitmin/makeitmin.io', // Your repository for archive comment
+    utterances: 'makeitmin/makeitmin.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -27,5 +27,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: 'G-YWXCRL7XRB', // Add your google analytics tranking ID
+  ga: 'G-F9HQEHDWHX', // Add your google analytics tranking ID
 }

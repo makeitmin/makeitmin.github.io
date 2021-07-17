51802cb6b489f57e4bf1e36e4f065989516bda05
SeongminHa <seongminha0910@gmail.com>
2021-07-17 17:22:43 +0000

    Feat: 기본 설정 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `makeitmin.io`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `Seongmin Ha`,
+  introduction: `휘발되지 않는 멋쟁이 개발자 되기`,
+  siteUrl: `https://makeitmin.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://www.github.com/makeitmin`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: ``, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'makeitmin/makeitmin.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '0', // Add your google analytics tranking ID
+  ga: 'G-BRWHDP9DGC', // Add your google analytics tranking ID
 }
